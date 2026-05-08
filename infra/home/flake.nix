bbe0b385cb5b066b6bb096f0992c3771caeeafe5
Jon Edwards <jkedwards@me.com>
2026-05-08 07:38:28 -0700

    feat(infra/home): nix-darwin system for Jons-MacBook-Pro

--- infra/home/flake.nix
+++ infra/home/flake.nix
@@ -4,6 +4,20 @@
   inputs = {
     kolohelios-nix.url = "https://flakehub.com/f/kolohelios/kolohelios-nix/*.tar.gz";
     nixpkgs.follows = "kolohelios-nix/nixpkgs";
+
+    # Pinned to the 25.11 release branches because nix-darwin and
+    # home-manager validate that their release matches the consumed
+    # Nixpkgs (currently 25.11 via kolohelios-nix). Bump in lockstep
+    # with the kolohelios-nix nixpkgs pin.
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin/nix-darwin-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
@@ -11,12 +25,22 @@
       self,
       kolohelios-nix,
       nixpkgs,
+      home-manager,
+      nix-darwin,
       ...
     }:
     let
       inherit (kolohelios-nix.lib) forEachSupportedSystem workflowPackages;
     in
     {
+      darwinConfigurations.Jons-MacBook-Pro = nix-darwin.lib.darwinSystem {
+        system = "aarch64-darwin";
+        modules = [
+          home-manager.darwinModules.home-manager
+          ./modules/darwin.nix
+        ];
+      };
+
       devShells = forEachSupportedSystem (
         { pkgs, ... }:
         {
